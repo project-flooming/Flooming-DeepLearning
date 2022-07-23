b5fbea394ef836060f5e2e2bb9e59626fd974fc6
sangh0 <hop7311@naver.com>
2022-07-23 14:50:54 +0900

    main function of classification task

--- classification_main.py
+++ classification_main.py
@@ -1,6 +1,4 @@
 import argparse
-from msilib.schema import Class
-from tabnanny import check
 
 import torch
 import torch.nn as nn
@@ -14,18 +12,12 @@ from train.classification_train import train_step
 
 def get_args_parser():
     parser = argparse.ArgumentParser(description='Set Pix2Pix training', add_help=False)
-    parser.add_argument('--path', default='C:/Users/user/MY_DL/flower/dataset/korean_flower/inputs', type=str,
+    parser.add_argument('--path', default='C:/Users/user/MY_DL/flower/dataset/flower_dataset/inputs', type=str,
                         help='Path of data')
     parser.add_argument('--img_size', default='256', type=int,
-                        help='Input size of VGG19 model')
+                        help='Input size of classification model')
     parser.add_argument('--device', default='cuda', type=str,
                         help='Set device')
-    parser.add_argument('--lr', default=0.0002, type=float,
-                        help='learning rate')
-    parser.add_argument('--beta1', default=0.9, type=float,
-                        help='beta1 of Adam optimizer')
-    parser.add_argument('--beta2', default=0.999, type=float,
-                        help='beta2 of Adam optimizer')
     parser.add_argument('--epoch', default=100, type=int)
     parser.add_argument('--batch_size', default=16, type=int)
     parser.add_argument('--num_classes', default=37, type=int)
@@ -67,6 +59,6 @@ def main(args):
     )
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('VGG19 training', parents=[get_args_parser()])
+    parser = argparse.ArgumentParser('MobileNetV3 training', parents=[get_args_parser()])
     args = parser.parse_args()
     main(args)
